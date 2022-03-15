650f6f16de26ac0cb6adb1ab33c406169d6ee99a
yorlend <yorlend@github.com>
2022-03-15 22:29:58 +0300

    lab3 init

---
+++ lab3/src/main/java/com/cg/lab3/AppController.java
@@ -0,0 +1,14 @@
+package com.cg.lab3;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Label;
+
+public class AppController {
+
+    @FXML
+    private Canvas canvas;
+
+    @FXML
+    private Label statusLabel;
+}
