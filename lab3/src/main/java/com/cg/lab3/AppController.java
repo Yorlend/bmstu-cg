a8501e3786146950c07093dc843d655411c5399f
yorlend <yorlend@github.com>
2022-03-21 00:45:35 +0300

    I and II parts done

--- lab3/src/main/java/com/cg/lab3/AppController.java
+++ lab3/src/main/java/com/cg/lab3/AppController.java
@@ -1,12 +1,20 @@
 package com.cg.lab3;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class AppController implements Initializable {
@@ -15,30 +23,161 @@ public class AppController implements Initializable {
     private Canvas canvas;
 
     @FXML
-    private Label statusLabel;
+    private ChoiceBox<String> algChoiceBox;
+
+    @FXML
+    private ChoiceBox<String> colorChoiceBox;
+
+    @FXML
+    private TextField xStartField;
+
+    @FXML
+    private TextField yStartField;
+
+    @FXML
+    private TextField xEndField;
+
+    @FXML
+    private TextField yEndField;
+
+    @FXML
+    private TextField rField;
+
+    @FXML TextField quantityField;
+
+    @FXML
+    protected void onSunButtonClicked() {
+
+        if (rField.getText().isEmpty() || quantityField.getText().isEmpty()) {
+
+            formErrorMessage("Поля ввода не могут быть пустыми");
+
+            return;
+        }
+
+        try {
+
+            int radius = (int) Math.round(Double.parseDouble(rField.getText()));
+            int quantity = (int) Math.round(Double.parseDouble(quantityField.getText()));
+
+            int xc = (int) canvas.getWidth() / 2;
+            int yc = (int) canvas.getHeight() / 2;
+
+            double theta = 0;
+            double delta = 2 * Math.PI / quantity;
+            for (int i = 0; i < quantity; i++) {
+                double sin = Math.sin(theta);
+                double cos = Math.cos(theta);
+
+                int x = (int) (radius * cos) + xc;
+                int y = (int) (radius * sin) + yc;
+
+                algFuncs.get(algChoiceBox.getValue()).accept(new Point(xc, yc), new Point(x, y),
+                        colors.get(colorChoiceBox.getValue()));
+
+                theta += delta;
+            }
+        } catch (NumberFormatException e) {
+
+        }
+    }
+
+    @FXML
+    protected void onDrawLineButtonClicked() {
+
+        if (xStartField.getText().isEmpty() || yStartField.getText().isEmpty() ||
+                xEndField.getText().isEmpty() || yEndField.getText().isEmpty()) {
+
+            formErrorMessage("Поля ввода не могут быть пустыми");
+
+            return;
+        }
+
+        try {
+
+            int xn = (int) Math.round(Double.parseDouble(xStartField.getText()));
+            int yn = (int) Math.round(Double.parseDouble(yStartField.getText()));
+            int xk = (int) Math.round(Double.parseDouble(xEndField.getText()));
+            int yk = (int) Math.round(Double.parseDouble(yEndField.getText()));
+
+            algFuncs.get(algChoiceBox.getValue()).accept(new Point(xn, yn), new Point(xk, yk),
+                    colors.get(colorChoiceBox.getValue()));
+        } catch (NumberFormatException e) {
+
+        }
+    }
+
+    @FXML
+    protected void onCleanButtonClicked() {
+
+        var gc = canvas.getGraphicsContext2D();
+
+        gc.clearRect(
+                0, 0,
+                canvas.getWidth(), canvas.getHeight()
+        );
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        Renderer renderer = new Renderer(canvas.getGraphicsContext2D().getPixelWriter());
+        renderer = new Renderer(canvas.getGraphicsContext2D().getPixelWriter());
 
-        int cx = (int) (canvas.getWidth() / 2), cy = (int) (canvas.getHeight() / 2);
-        int radius = 255;
-        int quantity = 100;
+        algChoiceBox.setItems(FXCollections.observableArrayList(
+                "ЦДА", "Брезенхем", "Целочис. Брезенхем", "Брезенхем с устранением ступенчатости",
+                "Ву", "Библиотечный"
+        ));
+        algChoiceBox.applyCss();
+        algChoiceBox.setValue("Библиотечный");
 
-        for (int i = 0; i < quantity; i++) {
-            double angle = 2 * Math.PI * i / quantity;
-            int xk = (int) (cx + radius * Math.cos(angle));
-            int yk = (int) (cy + radius * Math.sin(angle));
+        colorChoiceBox.setItems(FXCollections.observableArrayList(
+                "Черный", "Синий", "Красный", "Зеленый", "Цвет фона"
+        ));
+        colorChoiceBox.applyCss();
+        colorChoiceBox.setValue("Черный");
 
-            renderer.DDA(new Point(cx, cy), new Point(xk, yk), Color.BLACK);
-        }
+        algFuncs = Map.of(
+                "ЦДА", renderer::DDA,
+                "Брезенхем", renderer::BresenhamFloat,
+                "Целочис. Брезенхем", renderer::BresenhamInt,
+                "Брезенхем с устранением ступенчатости", renderer::BresenhamAA,
+                "Ву", renderer::Wu,
+                "Библиотечный", this::library
+        );
 
-        for (int i = 0; i < quantity; i++) {
-            double angle = 2 * Math.PI * i / quantity;
-            int xk = (int) (cx + radius * Math.cos(angle));
-            int yk = (int) (cy + radius * Math.sin(angle));
-
-            renderer.BresenhamFloat(new Point(cx, cy), new Point(xk, yk), Color.WHITE);
-        }
+        colors = Map.of(
+                "Черный", Color.BLACK,
+                "Синий", Color.BLUE,
+                "Красный", Color.RED,
+                "Зеленый", Color.GREEN,
+                "Цвет фона", Color.rgb(244, 244, 244)
+        );
     }
+
+    private void formErrorMessage(String text) {
+        Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+        errorAlert.setHeaderText("Некорректный ввод");
+        errorAlert.setContentText(text);
+        errorAlert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+        errorAlert.showAndWait();
+    }
+
+    private void library(Point start, Point end, Color color) {
+
+        var gc = canvas.getGraphicsContext2D();
+
+        gc.setImageSmoothing(false);
+        gc.setStroke(color);
+        gc.setLineWidth(1);
+
+        gc.beginPath();
+
+        gc.moveTo(start.getX() + 0.5, start.getY() + 0.5);
+        gc.lineTo(end.getX() + 0.5, end.getY() + 0.5);
+
+        gc.stroke();
+    }
+
+    private Renderer renderer;
+    private Map<String, TriConsumer<Point, Point, Color>> algFuncs;
+    private Map<String, Color> colors;
 }
--- lab3/src/main/java/com/cg/lab3/Renderer.java
+++ lab3/src/main/java/com/cg/lab3/Renderer.java
@@ -7,6 +7,7 @@ import javafx.scene.paint.Color;
 public class Renderer {
 
     private final PixelWriter pixelWriter;
+    public static final Color backgroundColor = Color.rgb(244, 244, 244);
 
     public Renderer(PixelWriter pixelWriter) {
         this.pixelWriter = pixelWriter;
@@ -37,12 +38,16 @@ public class Renderer {
         dy = Math.abs(dy);
 
         boolean swap = dy > dx;
-        double m = swap ? dx / dy : dy / dx;
+        if (swap) {
+            double tmp = dx;
+            dx = dy;
+            dy = tmp;
+        }
+        double m = dy / dx;
         double e = m - 0.5;
 
-        double limit = swap ? dy + 1 : dx + 1;
         int x = start.getX(), y = start.getY();
-        for (int i = 0; i < limit; i++) {
+        for (int i = 0; i < dx + 1; i++) {
             pixelWriter.setColor(x, y, color);
             if (e >= 0) {
                 if (swap) {
@@ -60,5 +65,162 @@ public class Renderer {
 
             e += m;
         }
+    }
+
+    public void BresenhamInt(Point start, Point end, Color color) {
+
+        int dx = end.getX() - start.getX(), dy = end.getY() - start.getY();
+        int sx = Integer.signum(dx), sy = Integer.signum(dy);
+        dx = Math.abs(dx);
+        dy = Math.abs(dy);
+
+        boolean swap = dy > dx;
+        if (swap) {
+            int tmp = dx;
+            dx = dy;
+            dy = tmp;
+        }
+        int e = 2 * dy - dx;
+
+        int x = start.getX(), y = start.getY();
+        for (int i = 0; i < dx + 1; i++) {
+            pixelWriter.setColor(x, y, color);
+            if (e >= 0) {
+                if (swap) {
+                    x += sx;
+                } else {
+                    y += sy;
+                }
+                e -= 2 * dx ;
+            }
+            if (swap) {
+                y += sy;
+            } else {
+                x += sx;
+            }
+
+            e += 2 * dy;
+        }
+    }
+
+    public void BresenhamAA(Point start, Point end, Color color) {
+
+        int I = 100;
+        /// TODO: проверять вырожденность
+
+        double dx = end.getX() - start.getX();
+        double dy = end.getY() - start.getY();
+
+        int sx = (int) Math.signum(dx);
+        int sy = (int) Math.signum(dy);
+
+        dx = Math.abs(dx);
+        dy = Math.abs(dy);
+
+        boolean swap = dy > dx;
+        if (swap) {
+            double tmp = dx;
+            dx = dy;
+            dy = tmp;
+        }
+        double m = dy / dx;
+        double e = I / 2.0;
+
+        int x = start.getX();
+        int y = start.getY();
+        m *= I;
+        double w = I - m;
+
+        for (int i = 0; i < dx + 1; i++) {
+            pixelWriter.setColor(x, y, getIntensiveColor(color, e, I));
+
+            if (e < w) {
+                if (swap) {
+                    y += sy;
+                } else {
+                    x += sx;
+                }
+
+                e += m;
+            } else {
+                x += sx;
+                y += sy;
+                e -= w;
+            }
+        }
+    }
+
+    private Color getIntensiveColor(Color color, double e, int I) {
+
+        double r = color.getRed() * 255;
+        double g = color.getGreen() * 255;
+        double b = color.getBlue() * 255;
+
+        double rb = 244, gb = 244, bb = 244;
+
+        double t = Math.floor(e) / I;
+
+        r += (rb - r) * t;
+        g += (gb - g) * t;
+        b += (bb - b) * t;
+
+        return Color.rgb((int) r, (int) g, (int) b);
+    }
+
+    public void Wu(Point start, Point end, Color color) {
+
+        int I = 255;
+
+        double dx = end.getX() - start.getX();
+        double dy = end.getY() - start.getY();
+
+        int sx = (int) Math.signum(dx);
+        int sy = (int) Math.signum(dy);
+
+        dx = Math.abs(dx);
+        dy = Math.abs(dy);
+
+        boolean swap = dy > dx;
+
+        if (swap) {
+            double tmp = dx;
+            dx = dy;
+            dy = tmp;
+
+            int stmp = sx;
+            sx = sy;
+            sy = stmp;
+        }
+
+        double m = dy / dx;
+
+        int X = swap ? start.getY() : start.getX();
+        double Y = swap ? start.getX() : start.getY();
+
+        for (int i = 0; i < dx + 1; i++) {
+            double r1 = I * fracPart(Y);
+            double r2 = I * revFracPart(Y);
+
+            int Yr = (int) Math.floor(Y);
+
+            if (swap) {
+                pixelWriter.setColor(Yr, X, getIntensiveColor(color, r1, I));
+                pixelWriter.setColor(Yr + 1, X, getIntensiveColor(color, r2, I));
+            } else {
+                pixelWriter.setColor(X, Yr, getIntensiveColor(color, r1, I));
+                pixelWriter.setColor(X, Yr + 1, getIntensiveColor(color, r2, I));
+            }
+
+            X += sx;
+            Y += sy * m;
+        }
+    }
+
+    private double fracPart(double num) {
+        return num - (long) num;
+    }
+
+    private double revFracPart(double num) {
+        return 1 - fracPart(num);
     }
 }
