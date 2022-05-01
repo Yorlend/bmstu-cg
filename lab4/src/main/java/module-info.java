3077055c06ab319e3a828644b9dbe24ef8899040
yorlend <yorlend@mail.ru>
2022-05-01 21:03:28 +0300

    fix module-info

--- lab4/src/main/java/module-info.java
+++ lab4/src/main/java/module-info.java
@@ -4,7 +4,6 @@ module com.cg.lab4 {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires org.kordamp.bootstrapfx.core;
 
     opens com.cg.lab4 to javafx.fxml;
     exports com.cg.lab4;
